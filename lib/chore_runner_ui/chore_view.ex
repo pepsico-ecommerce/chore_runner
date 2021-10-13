a04fc2b3c4d1c038da5700a72ee297c118cc1904
Chris Freeze <chrstnfreeze@gmail.com>
2021-10-13 11:47:29 -0500

    undo debug code

--- lib/chore_runner_ui/chore_view.ex
+++ lib/chore_runner_ui/chore_view.ex
@@ -3,8 +3,7 @@ defmodule ChoreRunnerUI.ChoreView do
   alias ChoreRunnerUI.Components.ChoreItemComponent
   @styles File.read!(Application.app_dir(:chore_runner, "priv/css/main.css"))
 
-  # defp styles, do: @styles
-  defp styles, do: File.read!(Application.app_dir(:chore_runner, "priv/css/main.css"))
+  defp styles, do: @styles
 
   defp first_log([{log, ts} | _]) do
     "[#{ts}] #{log}"
