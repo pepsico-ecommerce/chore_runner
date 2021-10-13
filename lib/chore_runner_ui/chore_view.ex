918b89908e1811eae581f234f1f6debf7bb09702
berkelmas <berkelmas96@gmail.com>
2021-10-13 12:33:11 +0300

    scss-watch, design implementation

--- lib/chore_runner_ui/chore_view.ex
+++ lib/chore_runner_ui/chore_view.ex
@@ -3,8 +3,8 @@ defmodule ChoreRunnerUI.ChoreView do
   alias ChoreRunnerUI.Components.ChoreItemComponent
   @styles File.read!(Application.app_dir(:chore_runner, "priv/css/main.css"))
 
-  defp styles, do: @styles
-  # defp styles, do: File.read!(Application.app_dir(:chore_runner, "priv/css/main.css"))
+  # defp styles, do: @styles
+  defp styles, do: File.read!(Application.app_dir(:chore_runner, "priv/css/main.css"))
 
   defp first_log([{ts, log} | _]) do
     "[#{ts}] #{log}"
