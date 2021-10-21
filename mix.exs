692c757599182da660f2f57792ae6454ea1753b8
Chris Freeze <chrstnfreeze@gmail.com>
2021-10-21 12:28:57 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ChoreRunner.MixProject do
   def project do
     [
       app: :chore_runner,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
